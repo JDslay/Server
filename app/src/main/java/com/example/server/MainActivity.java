305e211af5ebbfe9de775f6b66d2f6e610f9bf1e
Jürgen Dieterle <dieterle@irt.de>
2021-04-22 11:13:12 +0200

    Localization works

--- app/src/main/java/com/example/server/MainActivity.java
+++ app/src/main/java/com/example/server/MainActivity.java
@@ -1,37 +1,33 @@
 package com.example.server;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
-import androidx.dynamicanimation.animation.DynamicAnimation;
-
 import android.Manifest;
-import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationManager;
 import android.media.AudioManager;
 import android.os.Bundle;
-import android.telephony.CellSignalStrengthGsm;
 import android.telephony.SignalStrength;
 import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
-
 import com.example.server.my.Server01;
+import com.google.android.gms.common.internal.GmsLogger;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG= "MainActivity";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         String[] permissions = {Manifest.permission.READ_PHONE_STATE, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
 
-        ActivityCompat.requestPermissions(this, permissions, 0);
+        ActivityCompat.requestPermissions(this, permissions, 1001);
 
 
         //
@@ -61,7 +57,43 @@ public class MainActivity extends AppCompatActivity {
             // for ActivityCompat#requestPermissions for more details.
             return;
         }
-        Location lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+        Location loc = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+        Log.d(TAG, "onCreate: "+ loc.toString());
+
+        FusedLocationProviderClient fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            Task<Location> locationTask = fusedLocationProviderClient.getLastLocation();
+            locationTask.addOnSuccessListener(new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
+                    if (location != null){
+                        Log.d(TAG, "onSuccess: " + location.toString());
+                    }
+                    else{
+                        Log.d(TAG, "onSuccess: Location was null");
+                    }
+                }
+            });
+
+            locationTask.addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Log.e(TAG, "on failure: "+ e.getLocalizedMessage());
+                }
+            });
+        }
+
+
+
+
+
+
+
+
+
 
 
         Server01 server = new Server01(7000, audioManager, telephonyManager);
@@ -78,7 +110,7 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.btnLocation).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                tv.setText("Latitude is = " + lastKnownLocation.getLatitude());
+               tv.setText("Latitude is = " + loc.getLatitude());
             }
         });
 
